4f39e288a3cff0049472ce091ba393c9df8962a9
gabriel <gaamdal557@gmail.com>
2022-10-23 20:43:13 -0300

    commit inicial

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+
+            public IActionResult Carrossel()
+            {
+                return View();
+            }
+        }
+    }
+
+
